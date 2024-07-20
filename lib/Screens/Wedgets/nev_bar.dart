c5eabce43626690ad27769e6b8085019f6b62c43
campusproflutter <campusproFlutter>
2024-07-20 10:54:08 +0530

    feat(Bottom Navbar and dashboard merge done)

---
+++ lib/Screens/Wedgets/nev_bar.dart
@@ -0,0 +1,56 @@
+import 'package:campuspro/Utilities/colors.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+  const BottomNavBar(
+      {super.key, required this.currentIndex, required this.onTap});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Image.asset('assets/images/home.png', width: 24, height: 22),
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 3.0),
+            child: Image.asset('assets/images/switchuser.png',
+                width: 24, height: 24),
+          ),
+          label: 'User',
+        ),
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Image.asset('assets/images/chat.png', width: 24, height: 24),
+          ),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Image.asset('assets/images/helpdesk.png',
+                width: 24, height: 24),
+          ),
+          label: 'F&Q',
+        ),
+      ],
+      currentIndex: widget.currentIndex,
+      selectedItemColor: AppColors.appbuttonColor,
+      backgroundColor: Colors.white,
+      onTap: widget.onTap,
+    );
+  }
+}
--- lib/Screens/dashboard_screen.dart
+++ lib/Screens/dashboard_screen.dart
@@ -2,6 +2,8 @@
 
 import 'package:campuspro/Controllers/web_controller.dart';
 import 'package:campuspro/Screens/Wedgets/drawer.dart';
+import 'package:campuspro/Screens/Wedgets/nev_bar.dart';
+import 'package:campuspro/Utilities/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -15,9 +17,12 @@ class WebViewScreen extends StatelessWidget {
     return PopScope(
       canPop: true,
       child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Obx(() => Text(webController.appBarName.toString())),
+        appBar: customAppBar(),
+        bottomNavigationBar: Obx(
+          () => BottomNavBar(
+            currentIndex: webController.selectedBottomNavIndex.value,
+            onTap: webController.onItemTappedChangeBottomNavIndex,
+          ),
         ),
         drawer: AppDrawer(context),
         body: Obx(() {
@@ -31,6 +36,79 @@ class WebViewScreen extends StatelessWidget {
           }
         }),
       ),
+    );
+  }
+
+  AppBar customAppBar() {
+    final WebController webController = Get.find<WebController>();
+    return AppBar(
+      backgroundColor: AppColors.primarycolor, // Change to your desired color
+      centerTitle: false,
+      title: Obx(
+        () => Text(
+          webController.appBarName.toString(),
+          style: TextStyle(
+            color: Colors.white, // Change to your desired title color
+            fontSize: 20, // Change to your desired font size
+            fontWeight: FontWeight.bold, // Change to your desired font weight
+          ),
+        ),
+      ),
+      iconTheme: const IconThemeData(color: Colors.white),
+      actions: [
+        PopupMenuButton<String>(
+          icon: Image.asset(
+            'assets/images/person_icon.png',
+            width: 24,
+            height: 24,
+          ),
+          onSelected: (value) {},
+          itemBuilder: (BuildContext context) {
+            return [
+              const PopupMenuItem<String>(
+                value: 'change_password',
+                child: ListTile(
+                  leading: Icon(Icons.lock, color: AppColors.primarycolor),
+                  title: Text('Change Password'),
+                ),
+              ),
+              const PopupMenuItem<String>(
+                value: 'logout',
+                child: ListTile(
+                  leading: Icon(Icons.logout, color: AppColors.primarycolor),
+                  title: Text('Logout'),
+                ),
+              ),
+            ];
+          },
+        ),
+        PopupMenuButton<String>(
+          icon: Badge(
+            label: Text("1"),
+            child: const Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+          ),
+          onSelected: (String value) {
+            // Handle notification item selection
+            print('Selected: $value');
+          },
+          itemBuilder: (BuildContext context) {
+            return List<PopupMenuEntry<String>>.generate(
+              5,
+              (int index) => PopupMenuItem<String>(
+                value: 'Notification $index',
+                child: ListTile(
+                  leading: const Icon(Icons.notifications),
+                  title: Text('Notification $index'),
+                  subtitle: Text('this is detail $index'),
+                ),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
